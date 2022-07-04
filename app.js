4557a7100503e3f3dfbf471e1ba8aff6ce295338
Yusuf Eren <erenyusuf170@gmail.com>
2022-07-04 13:10:10 +0300

    Lesson1

--- app.js
+++ app.js
@@ -6,8 +6,17 @@ app.set('view engine', 'ejs');
 // Middlewares
 app.use(express.static('public'));
 
+// Routes
 app.get('/', (req, res) => {
-  res.status(200).send('Hi');
+  res.status(200).render('index', {
+    page_name: 'index',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.status(200).render('about', {
+    page_name: 'about',
+  });
 });
 
 const PORT = process.env.PORT || 3000;
