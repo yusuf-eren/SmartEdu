a12c37f36a6a79bba92aad9e09604844a7f98511
Yusuf Eren <erenyusuf170@gmail.com>
2022-07-04 12:26:16 +0300

    Lesson 1

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+
+// Middlewares
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.status(200).send('Hi');
 });
