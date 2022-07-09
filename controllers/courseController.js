816164e1ed01ad0a618ac810e13836ad278ad282
Yusuf Eren <erenyusuf170@gmail.com>
2022-07-09 19:38:35 +0300

    Student - Course Connection

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -3,7 +3,12 @@ const Category = require('../models/Category');
 
 exports.createCourse = async (req, res) => {
   try {
-    const course = await Course.create(req.body);
+    const course = await Course.create({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      user: req.session.userID,
+    });
     res.status(201).redirect('/courses');
   } catch (error) {
     res.status(400).json({
@@ -40,8 +45,9 @@ exports.getAllCourses = async (req, res) => {
 
 exports.getCourse = async (req, res) => {
   try {
-    const course = await Course.findOne({ slug: req.params.slug });
-
+    const course = await Course.findOne({ slug: req.params.slug }).populate(
+      'user'
+    );
     res.status(200).render('course', {
       course,
       page_name: 'courses',
